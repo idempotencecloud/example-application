13ed7b264946765023c2fed3465dd6f74b81c674
idempotencecloud <alex@idempotence.io>
2021-11-25 11:31:51 -0600

    Initial models

--- business_case/business_case/settings.py
+++ business_case/business_case/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'app.apps.AppConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -75,8 +76,12 @@ WSGI_APPLICATION = 'business_case.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'app',
+        'USER': 'root',
+        'PASSWORD': ']=}Q<C,o$b[I=.6<g4<l5@"cxO6r,h"7',
+        'HOST': '127.0.0.1',
+        'PORT': '3306',
     }
 }
 
