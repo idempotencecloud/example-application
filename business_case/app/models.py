eb8c9f5389936f8cdbd2f1a8ae6424d8535c269c
Asad Hasan <asadhasan@Asads-MacBook-Pro.local>
2021-11-25 11:21:30 -0600

    Add base models.

--- business_case/app/models.py
+++ business_case/app/models.py
@@ -1,3 +1,28 @@
 from django.db import models
 
 # Create your models here.
+class Company():
+    company_name = models.CharField(max_length=200)
+    company_contact_email = EmailField(max_length=254)
+    company_contact_phone = models.CharField(max_length=200)
+    accounts_receivable_email = EmailField(max_length=254)
+    company_type = models.CharField(
+        max_length=2,
+        choices=[
+		('SUPPLIER', 'Supplier'),
+		('VENDOR', 'Vendor'),
+		('CUSTOMER', 'Customer')
+	],
+        default='SUPPLIER',
+    )
+
+
+
+class Individual(models.Model):
+    first_name = models.CharField(max_length=200)
+    last_name = models.CharField(max_length=200)
+    phone_number = models.CharField(max_length=200)
+    interested_in_marketing_email = models.BooleanField()
+    customer_email = EmailField(max_length=254)
+    company = ForeignKey('Company',
+			 on_delete='models.CASCADE')
