358ffb205dd5c1a9ab761cd0ea941c497417de83
Asad <asadhasan@Asads-MacBook-Pro.local>
2022-09-19 05:54:37 -0500

    Add idempotenceAuthenticator view

--- business_case/app/views.py
+++ business_case/app/views.py
@@ -1,8 +1,24 @@
 from django.shortcuts import render
+#from django.core import serializers
 
 # Create your views here.
 from django.http import HttpResponse
+from django.http import JsonResponse
 
 
 def index(request):
     return HttpResponse("Hello, world. You're at the polls index.")
+
+def idempotenceAuthenticator(request):
+    if(not request.user.is_authenticated):
+        return JsonResponse({
+            'authenticated': False
+        })
+    #https://en.wikipedia.org/wiki/ISO_8601
+    #print(serializers.serialize("json", [request.user]))
+    return JsonResponse({
+        'legalName': "{} {}".format(request.user.first_name, request.user.last_name),
+        'authenticationTimestamp': request.user.last_login,
+        'userIdentifer': request.user.pk,
+        'authenticated': True
+    })
