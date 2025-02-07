f8a42f90fb9dc6228ae3084b22d1fa7325f588c1
idempotencecloud <alex@idempotence.io>
2025-02-07 14:35:35 -0600

    Add company creation form for example.

--- business_case/app/models.py
+++ business_case/app/models.py
@@ -2,6 +2,8 @@ from django.db import models
 
 # Create your models here.
 class Company(models.Model):
+    def __str__(self):
+        return self.company_name
     company_name = models.CharField(max_length=200)
     company_contact_email = models.EmailField(max_length=254)
     company_contact_phone = models.CharField(max_length=200)
@@ -19,6 +21,8 @@ class Company(models.Model):
 
 
 class Individual(models.Model):
+    def __str__(self):
+        return f"{self.first_name} {self.last_name}"
     first_name = models.CharField(max_length=200)
     last_name = models.CharField(max_length=200)
     phone_number = models.CharField(max_length=200)
--- business_case/app/views.py
+++ business_case/app/views.py
@@ -5,9 +5,32 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.http import JsonResponse
 
+from django.shortcuts import render
+from .models import Company, Individual
+from .forms import CompanyForm, IndividualForm
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+    if request.method == 'POST':
+        print(request.POST)
+        company_form = CompanyForm(request.POST)
+        if company_form.is_valid():
+            company_form.save()
+    else:
+        company_form = CompanyForm({
+            'company_name': 'Sample Company',
+            'company_contact_email': 'personnel@example.com',
+            'company_contact_phone': '1-800-555-5555',
+            'accounts_receivable_email': 'ar@example.com',
+            'company_type': 'CUSTOMER',
+        })
+
+
+    companies = Company.objects.all().order_by('-id')
+    context = {
+        'company_form': company_form,
+        'companies': companies,
+    }
+    return render(request, 'index.html', context)
 
 def idempotenceAuthenticator(request):
     if(not request.user.is_authenticated):
